9b1041af8619bb68be27a43005f258f00a057703
M-Pierron <130355886+M-Pierron@users.noreply.github.com>
2024-04-21 22:03:56 +0200

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,133 +1,55 @@
 <!DOCTYPE html> 
 <html> 
-	<head>
-		<link rel="stylesheet" type="text/css" href=".css">
-		<script type="text/javascript"src=".js"></script>
-	</head>
-	<body>
-        <table>
-            <tr>
-                <td>
-                    Adresse mail
-                </td>
-                <td>
-                    <?php 
-						
-                         echo $_SESSION["Adresse_mail"];
-                    ?>
-
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Mode de passe
-                </td>
-                <td>
-                     <?php 
-					 
-                         echo $_SESSION["Mot_de_passe"];
-                    ?>
-                </td>
-            </tr>
-			
-			<tr>
-                <td>
-                    Prenom
-                </td>
-                <td>
-                     <?php 
-					 
-                         echo $_SESSION["Prenom"];
-                    ?>
-                </td>
-            </tr>
-			
-			<tr>
-                <td>
-                    Nom
-                </td>
-                <td>
-                     <?php 
-					 
-                         echo $_SESSION["Nom"];
-                    ?>
-                </td>
-            </tr>
-			
-            <tr>
-                <td>
-                    Pseudo 
-                </td>
-                <td>
-                     <?php 
-					 
-                         echo $_SESSION["Pseudo"];
-                    ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Age
-                </td>
-                <td>
-                     <?php 
-					 
-                         echo $_SESSION["Age"];
-                    ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Sexe
-                </td>
-                <td>
-                    <?php 
-					
-                         echo $_SESSION["Sexe"];
-                    ?>
-                </td>
-            </tr>
-
-            <tr>
-                <td>
-                    Taille
-                </td>
-                <td>
-                    <?php 
-					
-                         echo $_SESSION["Taille"];
-                    ?>
-                </td>
-            </tr>
-
-            <tr>
-                <td>
-                    Poids
-                </td>
-                <td>
-                     <?php 
-					 
-                         echo $_SESSION["Poids"];
-                    ?>
-                </td>
-            </tr>
-
-            <tr>
-                <td>
-                    Centre d'interet
-                </td>
-                <td>
-                     <?php 
-					 
-                         echo $_SESSION["Centre_d_interet"];
-                    ?>
-                </td>
-            </tr>
-        </table>
-		<a href=profileedition.php> <img src="editing.jpg" alt="image"/> </a>
-		
-	</body> 
+<head>
+    <link rel="stylesheet" type="text/css" href=".css">
+    <script type="text/javascript" src=".js"></script>
+</head>
+<body>
+    <table>
+        <?php
+        if (isset($_SESSION["ID"])) {
+            $fichier_csv = fopen("donnees.csv", "r");
+            $id_session = $_SESSION["ID"];
+            $trouve = false;
+            while (($data = fgetcsv($fichier_csv, 1000, ";")) !== FALSE && !$trouve) {
+                if ($data[0] == $id_session) {
+                    $trouve = true;
+                    echo "<tr>";
+                    echo "<td>Adresse mail</td><td>$data[1]</td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td>Mode de passe</td><td>$data[2]</td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td>Prenom</td><td>$data[4]</td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td>Nom</td><td>$data[3]</td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td>Pseudo</td><td>$data[5]</td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td>Age</td><td>$data[7]</td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td>Sexe</td><td>$data[6]</td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td>Taille</td><td>$data[8]</td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td>Poids</td><td>$data[9]</td>";
+                    echo "</tr>";
+                    echo "<tr>";
+                    echo "<td>Centre d'interet</td><td>$data[10]</td>";
+                    echo "</tr>";
+                }
+            }
+            fclose($fichier_csv);
+        }
+        ?>
+    </table>
+    <a href="profileedition.php"> <img src="editing.jpg" alt="image"/> </a>
+</body> 
 </html>
-
-
-
