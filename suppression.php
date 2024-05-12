1c6b1a41c6db3e3bdbf1bb66258a73d8faddb123
M-Pierron <130355886+M-Pierron@users.noreply.github.com>
2024-05-12 17:36:26 +0200

    Delete suppression.php

--- suppression.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$fileold = "MonJambonbeurre.fr/users/$email_session";
-	$filenew = "MonJambonbeurre.fr/archive/users/$email_session";
-	rename($fileold, $filenew);
-	header("Location: accueil.php");
-	exit;
-?>
