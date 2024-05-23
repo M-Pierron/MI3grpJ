f5b8edc3eef4e37a407ef1ed570d447e335fdc69
Jolan21 <153626404+Jolan21@users.noreply.github.com>
2024-05-24 01:38:37 +0200

    Update and rename traitement_connection.php to traitement_connexion.php

---
+++ inscription/traitement_connexion.php
@@ -0,0 +1,38 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	session_start();
+	// Récupère les données du formulaire
+	$email = $_POST['Email']?? '';
+	$mot_de_passe = $_POST['mdp']??'';
+
+	$dossier=$email;
+	$fichier=$email.'/donnees_inscription.txt';
+	echo $email.'<br>';
+	echo $mot_de_passe.'<br>';
+	
+	if(is_dir($dossier) && file_exists($fichier)){
+		$contenu=file_get_contents($fichier);
+		$tab = explode("; ", $contenu);
+		echo $tab[0].'<br>';
+		echo $tab[1].'<br>';
+		echo $tab[2].'<br>';
+		
+		if ($email === $tab[0] && $mot_de_passe === $tab[1]){
+			$_SESSION['email'] = $email;
+			$_SESSION['loggedin'] = true;
+			$redirect_to = isset($_SESSION['redirect_to']) ? $_SESSION['redirect_to'] : 'https://cytech.cyu.fr/';
+				unset($_SESSION['redirect_to']); // Nettoie la session
+
+				// Redirige vers la page initiale
+				header("Location: $redirect_to");
+			}
+		else{
+			echo 'mdp incorrect';
+			}
+		}
+	else{
+		echo 'utilisateur pas trouvé';
+		}
+	}
+        	
+?>
