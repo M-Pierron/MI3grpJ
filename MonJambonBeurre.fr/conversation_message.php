4469ec35aa7cc3bb8b18e4d99594d31924c6ba96
M-Pierron <130355886+M-Pierron@users.noreply.github.com>
2024-05-26 23:45:33 +0200

    Update conversation_message.php

--- MonJambonBeurre.fr/conversation_message.php
+++ MonJambonBeurre.fr/conversation_message.php
@@ -6,6 +6,7 @@ if (!isset($_SESSION["email"])) {
 	hearder('Location: accueil.php'); 
 }
 
+// -- Recupere le recepteur avec la methode get --
 $recepteur = isset($_GET['recepteur']) ? htmlspecialchars($_GET['recepteur']) : null;
 ?>
 
@@ -17,14 +18,15 @@ $recepteur = isset($_GET['recepteur']) ? htmlspecialchars($_GET['recepteur']) : 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
+<! -- Sur la partie gauche de la page, affiche les discussions -->
     <div style="float: left; width: 30%;">
         <?php include("conversations.php"); ?>
     </div>
-
+<! -- Sur le reste, affiche la discussion en elle même -->
     <div style="float: left; width: 70%;">
         <?php if ($recepteur) : ?>
+	// -- Zone de message --
             <div id="messagesContainer"></div>
-
             <form id="messageForm" method="POST">
                 <textarea name="message" id="messageInput" required></textarea>
                 <input type="hidden" name="email" value="<?php echo htmlspecialchars($_SESSION['email']); ?>">
@@ -35,9 +37,10 @@ $recepteur = isset($_GET['recepteur']) ? htmlspecialchars($_GET['recepteur']) : 
             <p>Sélectionnez une conversation dans la liste à gauche pour afficher les messages.</p>
         <?php endif; ?>
     </div>
-
+	<! -- AJAX pour la messagerie instantanée -->
     <script>
         $(document).ready(function() {
+		// -- Recupere les messages dynamiquement --
             function recupMessages() {
                 var recepteur = '<?php echo $recepteur; ?>';
                 if (recepteur) {
@@ -50,7 +53,7 @@ $recepteur = isset($_GET['recepteur']) ? htmlspecialchars($_GET['recepteur']) : 
                     });
                 }
             }
-
+		// -- Envoie les messages dynamiquement -- 
             $('#messageForm').submit(function(e) {
                 e.preventDefault();
                 var formData = $(this).serialize();
@@ -66,8 +69,9 @@ $recepteur = isset($_GET['recepteur']) ? htmlspecialchars($_GET['recepteur']) : 
             });
 
             recupMessages();
+		// -- Choix arbitraire de 1 seconde --
             setInterval(recupMessages, 1000);
-
+		// -- Verification de supression --
             $(document).on('submit', '.deleteMessageForm', function(e) {
                 e.preventDefault();
                 var formData = $(this).serialize();
