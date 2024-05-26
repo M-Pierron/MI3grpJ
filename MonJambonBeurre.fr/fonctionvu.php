5fa04da0f39d0f44fd6dade7f552829d2a4372f8
M-Pierron <130355886+M-Pierron@users.noreply.github.com>
2024-05-27 00:22:12 +0200

    Update fonctionvu.php

--- MonJambonBeurre.fr/fonctionvu.php
+++ MonJambonBeurre.fr/fonctionvu.php
@@ -1,4 +1,12 @@
 <?php
+// -- Si ils sont bloqués, ou non abonnés, les rediriges --
+require_once('fonctionbloquer.php');
+require_once('fonction_abonnee.php');
+if (est_bloquer($_SESSION["email"], $_GET["email"]) || !(est_abonne($_SESSION["email"])) {
+    header('Location: accueil.php');
+    exit;
+    }
+
 // -- Inscrit l'utilisateur dans les vus de profil --
 function vuduprofil($email_recepteur, $email_session) {
     $fichier_log = "users/$email_recepteur/log_vu";
