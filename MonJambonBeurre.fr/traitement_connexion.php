fc82abfed8cf77d9cb3d2c8297d6e7b844f9db47
Adrien.frances <133261374+adrinox@users.noreply.github.com>
2024-05-26 23:35:13 +0200

    Update traitement_connexion.php

--- MonJambonBeurre.fr/traitement_connexion.php
+++ MonJambonBeurre.fr/traitement_connexion.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	session_start();
-	// Récupère les données du formulaire
+	
 	$email = $_POST['Email']?? '';
 	$mot_de_passe = $_POST['mdp']??'';
 	$_SESSION['email']= $email;
@@ -19,9 +19,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			$_SESSION['email'] = $email;
 			$_SESSION['loggedin'] = true;
 			$redirect_to = isset($_SESSION['redirect_to']) ? $_SESSION['redirect_to'] : 'accueil.php'; //page apres s'etre connecter
-				unset($_SESSION['redirect_to']); // Nettoie la session
+				unset($_SESSION['redirect_to']); 
 
-				// Redirige vers la page initiale
+				
 				header("Location: $redirect_to");
 			}
 		else{
