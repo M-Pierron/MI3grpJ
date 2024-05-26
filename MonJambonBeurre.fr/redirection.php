2b0bcada80e7490f7cbf8d4e4592d8fc3b326dac
Adrien.frances <133261374+adrinox@users.noreply.github.com>
2024-05-26 23:34:44 +0200

    Update redirection.php

--- MonJambonBeurre.fr/redirection.php
+++ MonJambonBeurre.fr/redirection.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 
-// Enregistre la page de départ dans la session
+
 if (!isset($_SESSION['redirect_to'])){
 	$_SESSION['redirect_to'] = $_SERVER['HTTP_REFERER'];
 }
 
-// Redirige vers la page de connexion
+
 header("Location: connexion.html");
 ?>
