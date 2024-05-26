636ffd908a690934d804ac9d93652572d67ee82a
M-Pierron <130355886+M-Pierron@users.noreply.github.com>
2024-05-26 23:56:28 +0200

    Update fonctionvu.php

--- MonJambonBeurre.fr/fonctionvu.php
+++ MonJambonBeurre.fr/fonctionvu.php
@@ -1,4 +1,5 @@
 <?php
+// -- Inscrit l'utilisateur dans les vus de profil --
 function vuduprofil($email_recepteur, $email_session) {
     $fichier_log = "users/$email_recepteur/log_vu";
     if (!file_exists($fichier_log)) {
