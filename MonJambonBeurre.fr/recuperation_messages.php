38c55a09fae39a8c99ce0f936f6d76f9d36fd93a
M-Pierron <130355886+M-Pierron@users.noreply.github.com>
2024-05-27 00:50:54 +0200

    Update recuperation_messages.php

--- MonJambonBeurre.fr/recuperation_messages.php
+++ MonJambonBeurre.fr/recuperation_messages.php
@@ -6,12 +6,12 @@ if (!isset($_SESSION["email"])) {
     exit;
 }
 // -- Si ils sont bloqués, ou non abonnés, les rediriges --
-require_once('fonctionbloquer.php');
+/*require_once('fonctionbloquer.php');
 require_once('fonction_abonnee.php');
-if (est_bloquer($_SESSION["email"], $_GET["email"]) || !(est_abonne($_SESSION["email"]) || !(est_femme($_SESSION["email"]) {
+if (est_bloquer($_SESSION["email"], $_GET["email"]) || !(est_abonne($_SESSION["email"]) || est_femme($_SESSION["email"]))) {
     header('Location: accueil.php');
     exit;
-    }
+}*/
                                                                                              
 $envoyeur = htmlspecialchars($_GET["email"]);
 $recepteur = htmlspecialchars($_GET['recepteur']);
